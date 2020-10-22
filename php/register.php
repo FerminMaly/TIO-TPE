fc9c2746cace068207d19d4db5b56ffb390c47f5
Fermin Maly Varni <ferminmalyvarni@gmail.com>
2020-10-22 15:31:47 -0300

    Modifique funcion register.php y agregue imagen de gir Reset HEAD

--- php/register.php
+++ php/register.php
@@ -1,6 +1,6 @@
 <?php
 
-    function Register(){
+    function Registrarse(){
         if (!isset($_POST['usuario']) && (($_POST['password']) != "1234"))
 
         print("Te registraste con exito");
